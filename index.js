c808621593fe49ecdf44cf82dc97683cef955d8a
Scott Henshaw <shenshaw@vfs.com>
2023-01-03 12:34:14 -0800

    Weeks 1 - 3 base

--- index.js
+++
@@ -1,8 +0,0 @@
-// Copyright (C) 2022 Scott Henshaw
-
-import App from './scripts/App.js'
-
-document.addEventListener('DOMContentLoaded', event => {
-    // Our code goes here....
-    const app = new App();
-})

47fd4fe0d85e8bc622af3f6d834bb5dd9ebd750a
Scott Henshaw <shenshaw@vfs.com>
2023-01-03 12:52:42 -0800

    Includes all css (grid, flex) and basic JS

--- scripts/App.js
+++ scripts/App.js
@@ -2,46 +2,15 @@
 
 export default class App {
 
-    #dialogState;
-
     constructor() {
         // this is where we build an instance of this app
-        this.#dialogState = false;
 
         // handle a simple button press
         document.querySelector("#press-me")
-            .addEventListener("click", event =>
-                this.openDialog("Opened the dialog")
-            );
-
-        document.querySelector("#close-me")
             .addEventListener("click", event => {
-                // when the user presses the button...
-                this.closeDialog("Closed the dialog")
+
+                const el = document.querySelector("#results")
+                el.innerHTML ="The user presses the button..."
             })
     }
-
-
-    openDialog( msg = "opened") {
-
-        if (!this.#dialogState)
-            return;
-
-        this.#dialogState = !this.#dialogState;
-        document.querySelector("#result-dlg").show();
-        console.log( msg )
-    }
-
-
-    closeDialog( msg = "closed") {
-
-        if (this.#dialogState)
-            return;
-
-        this.#dialogState = !this.#dialogState;
-        document.querySelector("#result-dlg").close()
-        console.log( msg )
-    }
-
-
 }
